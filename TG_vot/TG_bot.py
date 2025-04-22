5a8ef25e5ed5cfb242130a2fc4f07ddf2a9ead09
Yugay Maxim <yugaymax@gmail.com>
2025-04-22 17:04:55 +0500

    lol

--- TG_vot/TG_bot.py
+++ TG_vot/TG_bot.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import openai
 
-load_dotenv("E:/Python_Basic/PythonProject13/TG_vot/.env")
+load_dotenv("/.env")
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
